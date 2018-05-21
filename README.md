934e275e5044207c034580a17651fb431abead0a
Tiago Seabra <tlgs@users.noreply.github.com>
2018-05-21 16:48:22 +0200

    mass simplification

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 Koala-fy is a super simple Chrome Extension that replaces *koalafiable* text pieces with the :koala: emoji.
 
 It is meant as a fun project and should be taken as such. You can download it from the Chrome Web Store [here](https://chrome.google.com/webstore/detail/koala-fy/jpdeekpjokblfbaolmgnkegmgfnfjime?).
-
-
-Want to help out? Feel free to fork it and make a pull request!
---
+++ content.js
@@ -0,0 +1,14 @@
+var pattern = new RegExp(/[kcq][uo]al[ai]h?/, "gi");
+var emoji = String.fromCodePoint(0x1F428);
+
+function hunt(parent){
+    for(var i = 0; i < parent.childNodes.length; i++){
+        var node = parent.childNodes[i];
+        if(node.nodeType == Element.TEXT_NODE)
+            node.textContent = node.textContent.replace(pattern, emoji);
+        else if(node.nodeType == Element.ELEMENT_NODE)
+            hunt(node);
+    }
+}
+
+hunt(document.body);
